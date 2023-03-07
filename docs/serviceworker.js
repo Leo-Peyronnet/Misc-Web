9e49d206a177ef49c7c6a73ace3f7fe63fcb6f31
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-03-07 13:20:12 +0100

    Updated manifest

--- docs/serviceworker.js
+++ docs/serviceworker.js
@@ -1,19 +1,55 @@
-let staticCacheName = "pwa";
+var GHPATH = '/Misc-Web';
+var APP_PREFIX = 'gppwa_';
+var VERSION = 'version_001';
+var URLS = [
+    `${GHPATH}/`,
+    `${GHPATH}/chat.html`,
+    `${GHPATH}/css/style.css`,
+    `${GHPATH}/img/dark-landscape.png`,
+    `${GHPATH}/img/dark-landscape2.png`,
+    `${GHPATH}/js/form.js`,
+    `${GHPATH}/js/cita.js`,
+    `${GHPATH}/js/chat.js`
+]
 
-self.addEventListener("install", function (e) {
+var CACHE_NAME = APP_PREFIX + VERSION
+self.addEventListener('fetch', function (e) {
+    console.log('Fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('Responding with cache : ' + e.request.url);
+                return request
+            } else {
+                console.log('File is not cached, fetching : ' + e.request.url);
+                return fetch(e.request)
+            }
+        })
+    )
+})
+
+self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open(staticCacheName).then(function (cache) {
-            return cache.addAll(["/"]);
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('Installing cache : ' + CACHE_NAME);
+            return cache.addAll(URLS)
         })
-    );
-});
+    )
+})
 
-self.addEventListener("fetch", function (event) {
-    console.log(event.request.url);
-
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            var cacheWhitelist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX)
+            })
+            cacheWhitelist.push(CACHE_NAME);
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    console.log('Deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i])
+                }
+            }))
         })
-    );
-});
+    )
+})
