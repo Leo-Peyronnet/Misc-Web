1dd9fe91ddb3c581b3295829a9d161a40ed37879
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-03-11 18:40:21 +0100

    Update chat.js

--- docs/js/chat.js
+++ docs/js/chat.js
@@ -70,7 +70,7 @@ function send(msg) {
             model: "gpt-3.5-turbo",
             messages: conv,
             temperature: 0.97,
-            max_tokens: 128,
+            max_tokens: 256,
             top_p: 1,
             frequency_penalty: 0,
             presence_penalty: 0.6,
